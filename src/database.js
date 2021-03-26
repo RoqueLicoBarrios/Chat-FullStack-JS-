f8678d6b0e1c1d96d8007f6b5f66cbdc5206af60
Roque Lico Barrios <roque.lico@sisorg.com.ar>
2021-03-25 21:15:07 -0300

    ultimo...

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://Roque:Esperma93@chatdb.unpat.mongodb.net/ChatDB?retryWrites=true&w=majority',  {
+mongoose.connect('mongodb://Roque:Esperma93@chatdb-shard-00-00.unpat.mongodb.net:27017,chatdb-shard-00-01.unpat.mongodb.net:27017,chatdb-shard-00-02.unpat.mongodb.net:27017/ChatDB?ssl=true&replicaSet=atlas-aj4fce-shard-0&authSource=admin&retryWrites=true&w=majority',  {
     useNewUrlParser: true
 })
   .then(db => console.log('db connected'))
